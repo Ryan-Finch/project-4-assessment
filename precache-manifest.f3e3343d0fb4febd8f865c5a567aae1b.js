57a8a77257967784542c80bbbdf6a457e219da00
ryanwfin <ryan.finch4416@gmail.com>
2020-04-15 14:32:58 -0600

    Updates

---
+++ precache-manifest.f3e3343d0fb4febd8f865c5a567aae1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1db2672499e338df14c6a80c8ffd7f09",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "b70b8ed73c6d068d0dd2",
+    "url": "/project-4-assessment/static/css/main.ecdd64a6.chunk.css"
+  },
+  {
+    "revision": "bc26d90bf71caa0be6ae",
+    "url": "/project-4-assessment/static/js/2.08137a7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.08137a7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b70b8ed73c6d068d0dd2",
+    "url": "/project-4-assessment/static/js/main.75de0418.chunk.js"
+  },
+  {
+    "revision": "bb8b828653ad1446ab40",
+    "url": "/project-4-assessment/static/js/runtime-main.58f97c80.js"
+  }
+]);
